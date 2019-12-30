1ec102f5e5ee13ecae55b33d27cbd66394fc15c1
moreira.kd@outlook.com <moreira.kd@outlook.com>
2019-12-30 10:08:31 -0500

    knexfile changes

--- knexfile.js
+++ knexfile.js
@@ -4,41 +4,21 @@ module.exports = {
 
   development: {
     client: 'sqlite3',
+    useNullAsDefault: true,
     connection: {
-      filename: './dev.sqlite3'
-    }
+      filename: './data/dev.db3'
+    }    
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
+  pool: {      
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign keys = ON', done);
+      }
     },
     migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
-  production: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
+      directory: './data/migrations'
     },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
+    seeds: {
+      directory: './data/seeds'
     }
-  }
-
-};
+  };
