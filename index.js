8a7fbcca0f95ff3cfa903c8bac8efd0eabd0856a
moreira.kd@outlook.com <moreira.kd@outlook.com>
2020-01-03 12:10:24 -0500

    removed sessions from index.js

--- index.js
+++ index.js
@@ -1,45 +1,11 @@
 const express = require ('express');
 const cors = require('cors');
-const session = require('express-session');
-const knexSessionStore = require('connect-session-knex')(session);
-const db = require('./data/db.js');
-
-const sessionConfig = {
-    secret: process.env.SESSION_SECRET || 'test secret',    
-    name: 'cookie monster',
-    cookie: {
-        maxAge: 1000 * 60 * 60 * 24,
-        // maxAge is set in milliseconds
-        // 1000 ms === 1 sec * 60 === 1 min * 60 === 1 hr * 24 === 24 hrs
-        secure: process.env.NODE_ENV === "production" ? true : false,
-        httpOnly: true,
-    },
-        
-    resave: false,
-    // determines whether or not we save the cookie again on multiple visits
-    saveUninitialized: false,
-    // if true we would be giving everyone a cookie, whether they are logged in or not
-    
-    store: new knexSessionStore({
-        knex: db,
-        tablename: 'knexsessions',
-        sidfieldname: 'sessionid',
-        createtable: true,
-        clearInterval: 1000 * 60 * 30,
-    })
-};
 
 const server = express();
-server.use(cors({
-
-    credentials: true,
-    origin: 'http://localhost:3000',
-    // origin address will change on deployment will be FE launched url  
-
-}));
+server.use(cors());
 
 server.use(express.json());
-server.use(session(sessionConfig));
+
 
 const authRouter = require('./auth/auth-router.js');
 server.use('/auth', authRouter);
